422cdf4e61c1039fe5c92782207418bb047b5e60
JustGirl2019 <56073254+JustGirl2019@users.noreply.github.com>
2020-04-28 22:59:33 +0300

    Init

---
+++ assets/src/js/main.js
@@ -0,0 +1,12 @@
+// Импортируем jQuery
+//= ../../../node_modules/jquery/dist/jquery.js
+
+// Импортируем Popper
+// ../../../node_modules/popper.js/dist/umd/popper.js
+
+// проверка формы и различные ее стили
+// form.js
+
+//= slick.min.js
+
+//= script.js
